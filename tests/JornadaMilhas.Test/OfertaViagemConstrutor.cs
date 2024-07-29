b08a680fcab08def1a2e2fab82e4934101b05bb6
jonatas-campista <jonatasc5622@gmail.com>
2024-07-29 17:36:35 -0300

    colocando um padrao de nomeclatura

--- tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
+++ tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -50,5 +50,21 @@ namespace JornadaMilhas.Test
             Assert.Contains("Erro: Data de ida não pode ser maior que a data de volta.", oferta.Erros.Sumario);
             Assert.False(oferta.EhValido);
         }
+
+        [Fact]
+        public void TestandoOfertaComPriceMenorQueZero()
+        {
+            //cenario
+            Rota rota = new Rota("origemTeste", "destinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = -1;
+
+            //ação
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            //validação
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
     }
 }
