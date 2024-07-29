eef2bbf2618231283b97befa431db2e334d2c13a
jonatas-campista <jonatasc5622@gmail.com>
2024-07-29 14:50:21 -0300

    testando oferta valida

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -1,11 +1,20 @@
+using JornadaMilhasV1.Modelos;
+
 namespace JornadaMilhas.Test
 {
     public class OfertaViagemTest
     {
         [Fact]
-        public void TesteValidandoOferta()
+        public void TestandoOfertaValida()
         {
+            Rota rota = new Rota("origemTeste","destinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+            var validacao = true;
 
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Equal(validacao,oferta.EhValido);
         }
     }
 }
